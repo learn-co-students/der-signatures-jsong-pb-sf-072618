f59df180c2849778424f454d1424ba177568bf04
adamjonas <jonas@flatironschool.com>
2018-06-23 19:49:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# DER Signatures
+
+Another class that we need to learn to serialize are signatures. Much like the SEC format, it needs to encode two different numbers, r and s. Unfortunately, unlike S256Point, Signature cannot be compressed as s cannot be derived solely from r.
+
+The standard for serializing signatures is called DER format. DER stands for ... and was used by Satoshi to create Bitcoin. This was most likely because the standard was already defined in 2008 and it was easy enough to adopt, rather than creating a new standard.
+
+DER Signatures are created like this:
+
+1. Start with the 0x30 byte
+2. Encode the length of the rest of the signature (usually 0x44 or 0x45) and append
+3. Append the marker byte (0x02)
+4. Encode r as a big endian integer, but prepend with 0x00 byte if r's first byte >= 0x80. Add this to the result
+5. Append the marker byte (0x02)
+6. Encode s as a big endian integer, but prepend with 0x00 byte if s's first byte >= 0x80. Add this to the result
+
+Because we know r is a 256-bit integer, r will be at most 32-bytes expressed as big-endian. It's also possible the first byte could be >= 0x80, so part 4 can be at most 33-bytes. However, if r is a relatively small number, it could be less than 32 bytes. Same goes for s and part 6.
+
+Here's how this is coded in Python:
+
+```python
+class Signature:
+
+    def der(self):
+        rbin = self.r.to_bytes(32, byteorder='big')
+        # remove all null bytes at the beginning
+        rbin = rbin.lstrip(b'\x00')
+        # if rbin has a high bit, add a \x00
+        if rbin[0] & 0x80:
+            rbin = b'\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin  # <1>
+        sbin = self.s.to_bytes(32, byteorder='big')
+        # remove all null bytes at the beginning
+        sbin = sbin.lstrip(b'\x00')
+        # if sbin has a high bit, add a \x00
+        if sbin[0] & 0x80:
+            sbin = b'\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
+```
+<1> In Python 3, you can convert a list of numbers to the byte equivalents using bytes([some_integer1, some_integer2])
+
+Overall, this is an inefficient way to encode r and s as there are at least 4 bytes that aren't necessary.
+
+```python
+from ecc import S256Point, Signature
+from helper import double_sha256
+
+der = bytes.fromhex('304402201f62993ee03fca342fcb45929993fa6ee885e00ddad8de154f268d98f083991402201e1ca12ad140c04e0e022c38f7ce31da426b8009d02832f0b44f39a6b178b7a1')
+sec = bytes.fromhex('0204519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574')
+
+# message is the double_sha256 of the message "ECDSA is awesome!"
+z = int.from_bytes(double_sha256(b'ECDSA is awesome!'), 'big')
+
+# parse the der format to get the signature
+# parse the sec format to get the public key
+
+# use the verify method on S256Point to validate the signature
+```
---
+++ tx.py
@@ -0,0 +1,90 @@
+from io import BytesIO
+from unittest import TestCase
+
+from helper import (
+    little_endian_to_int,
+    read_varint,
+)
+from script import Script
+
+
+class Tx:
+
+    def __init__(self, version, tx_ins, tx_outs, locktime):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+        tx_outs = ''
+        for tx_out in self.tx_outs:
+            tx_outs += tx_out.__repr__() + '\n'
+        return 'version: {}\ntx_ins:\n{}\ntx_outs:\n{}\nlocktime: {}\n'.format(
+            self.version,
+            tx_ins,
+            tx_outs,
+            self.locktime,
+        )
+
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig, sequence):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        self.script_sig = Script.parse(script_sig)
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
+
+
+class TxOut:
+
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = Script.parse(script_pubkey)
+
+    def __repr__(self):
+        return '{}:{}'.format(self.amount, self.script_pubkey)
+
+
+class TxTest(TestCase):
+
+    def test_parse_inputs(self):
+        raw_tx = bytes.fromhex('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
+        stream = BytesIO(raw_tx)
+        tx = Tx.parse(stream)
+        self.assertEqual(len(tx.tx_ins), 1)
+        want = bytes.fromhex('d1c789a9c60383bf715f3f6ad9d14b91fe55f3deb369fe5d9280cb1a01793f81')
+        self.assertEqual(tx.tx_ins[0].prev_tx, want)
+        self.assertEqual(tx.tx_ins[0].prev_index, 0)
+        want = bytes.fromhex('483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278a')
+        self.assertEqual(tx.tx_ins[0].script_sig.serialize(), want)
+        self.assertEqual(tx.tx_ins[0].sequence, 0xfffffffe)
+
+    def test_parse_outputs(self):
+        raw_tx = bytes.fromhex('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
+        stream = BytesIO(raw_tx)
+        tx = Tx.parse(stream)
+        self.assertEqual(len(tx.tx_outs), 2)
+        want = 32454049
+        self.assertEqual(tx.tx_outs[0].amount, want)
+        want = bytes.fromhex('76a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac')
+        self.assertEqual(tx.tx_outs[0].script_pubkey.serialize(), want)
+        want = 10011545
+        self.assertEqual(tx.tx_outs[1].amount, want)
+        want = bytes.fromhex('76a9141c4bc762dd5423e332166702cb75f40df79fea1288ac')
+        self.assertEqual(tx.tx_outs[1].script_pubkey.serialize(), want)
+
+    def test_parse_locktime(self):
+        raw_tx = bytes.fromhex('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
+        stream = BytesIO(raw_tx)
+        tx = Tx.parse(stream)
+        self.assertEqual(tx.locktime, 410393)
